4bbce26bab516c5a46ab5191ada2ae698d2cfa76
Craig Bucher <97258442+craigbucher@users.noreply.github.com>
2022-12-29 16:40:54 -0500

    new logo

--- src/components/FirstComponent.tsx
+++ src/components/FirstComponent.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-let Logo ="https://logrocket-assets.io/static/home-hero-c97849b227a3d3015730e3371a76a7f0.svg";
+let Logo = "./logo.svg"
+// let Logo ="https://logrocket-assets.io/static/home-hero-c97849b227a3d3015730e3371a76a7f0.svg";
 export default class FirstComponent extends React.Component <{}> {
   render() {
     return (
@@ -7,6 +8,7 @@ export default class FirstComponent extends React.Component <{}> {
         <h3>A Simple React Component Example with Typescript</h3>
         <div>
           <img height="250" src={Logo} /> 
+          {/* <img height="250" src="./logo.svg" /> */}
         </div>
         <p>This component shows the Logrocket logo.</p>
         <p>For more info on Logrocket, please visit https://logrocket.com </p>
